98962b8a4dff217a9449825fb7498ffdd3ac73bc
G0rg0ne <reda1999nox@gmail.com>
2024-07-07 03:37:20 +0200

    feat: :sparkles: add replicate

--- src/master_planner/main.py
+++ src/master_planner/main.py
@@ -1,15 +1,23 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
- 
-# The device to load the model onto. 
-#
-# Available device types:
-# "cuda" - NVIDIA GPU
-# "cpu" - Plain CPU
-# "mps" - Apple silicon
-device = "cpu"
-access_token = "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"
+import replicate
 
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1", token=access_token)
-model.to(device)
- 
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.1", token=access_token)
+f = open("info.txt", "r")
+str_file = f.read()
+promt = "when did spain score the goals?"
+context = " using this context"
+
+# The mistralai/mixtral-8x7b-instruct-v0.1 model can stream output as it's running.
+for event in replicate.stream(
+    "mistralai/mixtral-8x7b-instruct-v0.1",
+    input={
+        "top_k": 50,
+        "top_p": 0.9,
+        "prompt":  promt + context + str_file ,
+        "temperature": 0.6,
+        "system_prompt": "You are a very helpful, answer using one phrase",
+        "length_penalty": 1,
+        "max_new_tokens": 1024,
+        "prompt_template": "<s>[INST] {prompt} [/INST] ",
+        "presence_penalty": 0
+    },
+):
+    print(str(event), end="")
